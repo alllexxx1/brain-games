4db3f21b9d317d431b52ca2f9f8ad43e88494cce
alllexxx1 <alekkorpachyov@gmail.com>
2023-03-26 21:21:31 +0300

    Fix brain-progression mistakes

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,7 +5,7 @@ rules = 'What number is missing in the progression?'
 
 
 def initialize_game():
-    progression = list(range(randint(0, 10), randint(30, 40), randint(2, 6)))
+    progression = list(range(randint(0, 5), randint(30, 40), randint(2, 6)))
     hidden_index = randint(0, len(progression) - 1)
     right_answer = str(progression[hidden_index])
     progression[hidden_index] = ".."
