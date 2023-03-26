9e3f0c4395f5e6e034419ada7adfac097aa608c3
alllexxx1 <alekkorpachyov@gmail.com>
2023-03-26 21:51:53 +0300

    Fix brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,15 +4,22 @@ from random import randint
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def initialize_game():
-    number = randint(0, 100)
+def is_prime(number):
     if number % 2 == 0 or number <= 1:
-        correct_answer = 'no'
+        return False
     elif number == 2:
-        correct_answer = 'yes'
+        return True
     else:
         for i in range(3, int(number ** 0.5) + 1, 2):
             if number % i == 0:
-                correct_answer = 'no'
+                return False
+        return True
+
+
+def initialize_game():
+    number = randint(0, 100)
+    if is_prime(number):
         correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return (number, correct_answer)
